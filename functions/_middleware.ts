9c75ca2f13ea7262b517ef9c653ba00caaa8dc16
asce0110 <asce0110@github.com>
2025-03-29 13:19:23 +0800

    添加Cloudflare Pages部署配置: 指定Node.js版本，添加Wrangler配置，解决依赖兼容性问题

---
+++ functions/_middleware.ts
@@ -0,0 +1,13 @@
+import { Context } from "@cloudflare/workers-types";
+
+export const onRequest = async (context: Context) => {
+  // 增加头部，允许更大的请求体
+  const response = await context.next();
+  
+  // 添加CORS头
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, X-Request-ID");
+  
+  return response;
+}; 
